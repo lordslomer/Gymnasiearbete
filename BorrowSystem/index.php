c1478bd5bda8d688ae35909fba4099cff98069fa
SalamSki <48717734+lordslomer@users.noreply.github.com>
2020-01-25 02:05:38 +0100

    progress

--- BorrowSystem/index.php
+++ BorrowSystem/index.php
@@ -1,37 +1,44 @@
 <!DOCTYPE html>
 <html>
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width = device-width, initial-scale = 1.0">
-        <title> Main </title>
-    </head>
-    
-    <body>
-        <button onclick="DisplayBorrowBlock()">Låna</button>
-        <button onclick="DisplayReturnBlock()">Lämna</button>
-        <P Id="Borrow" >Här Lånar Man</P>
-        <P Id="Return">Här lämnar Man</P>
-        
-        <script>
-            function DisplayBorrowBlock() {
-                var BorrowBlock= document.getElementById("Borrow");
-                if (BorrowBlock.style.display === "none"){
-                    BorrowBlock.style.display = "block";
-                }
-                else{
-                    BorrowBlock.style.display = "none";
-                }
+
+<head>
+    <meta charset="UTF-8">
+    <link rel="stylesheet" type="text/css" href="style.css">
+    <title> Main </title>
+</head>
+
+<body>
+    <div id="Container">
+        <form>
+            <button onclick="DisplayBorrowBlock()">Låna</button>
+            <button onclick="DisplayReturnBlock()">Lämna</button>
+            <P Id="Borrow">Här Lånar Man</P>
+            <P Id="Return">Här lämnar Man</P>
+        </form>
+        <form>
+            <button></button>
+        </form>
+    </div>
+    <script>
+        function DisplayBorrowBlock() {
+            var BorrowBlock = document.getElementById("Borrow");
+            if (BorrowBlock.style.display === "none") {
+                BorrowBlock.style.display = "block";
+            } else {
+                BorrowBlock.style.display = "none";
             }
-            
-            function DisplayReturnBlock() {
-                var ReturnBlock = document.getElementById("Return");
-                if(ReturnBlock.style.display === "none"){
-                    ReturnBlock.style.display = "block";
-                }
-                else{
-                    ReturnBlock.style.display = "none";
-                }
+        }
+
+        function DisplayReturnBlock() {
+            var ReturnBlock = document.getElementById("Return");
+            if (ReturnBlock.style.display === "none") {
+                ReturnBlock.style.display = "block";
+            } else {
+                ReturnBlock.style.display = "none";
             }
-</script>   
-    </body>
-</html> 
+        }
+
+    </script>
+</body>
+
+</html>
