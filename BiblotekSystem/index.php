215a9441a22735fde38c3282902e7bd5eedc0aaa
SalamSki <48717734+lordslomer@users.noreply.github.com>
2020-02-03 18:01:55 +0100

    Working Progress!!!!

--- BiblotekSystem/index.php
+++ BiblotekSystem/index.php
@@ -5,7 +5,25 @@
 
 
 <div class="Container">
-
+    <div class="SubSearchBox">
+        <form>
+            <span>Sök:</span>
+            <input type="text" size="40">
+            <span class="StyledSpans">Go</span>
+        </form>
+    </div>
+    <div class="SearchBox">
+        <h1>Katalog</h1>
+        <form>
+            <span>Sök:</span>
+            <input type="text" size="40">
+            <span class="StyledSpans">Go</span>
+        </form>
+    </div>
+    <div class="LoginBox">
+        <span>Logga in här : </span>
+        <span class="StyledSpans">Login</span>
+    </div>
 </div>
 
 
